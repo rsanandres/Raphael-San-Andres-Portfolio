9ccfdbf180cc9302e719cc1dc629ae12efff75a5
Raphael San Andres <rsanandres@ucla.edu>
2022-10-03 15:33:04 -0700

    accidentally uploaded tokens

--- Rbiot Discord Bot with Cohere.ai/token_2.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct  2 20:44:45 2022
-
-@author: quort
-"""
-
-TOKEN = "MTAyNTkwNTMyOTE2NDI3MTY4OA.Gh2_DK.3NkK1IQrtThwS6MMhPhFRMWAIR2QAIQYvpMMyY"
-TOKEN_J = "Kd4crvwDI44CChvBIvAEpal4sDw0THivEi7bVH6p"
